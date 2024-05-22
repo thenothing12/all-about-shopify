143d3774f9029344d67433d1fd1dad89b4e96500
the nothing <mydeadass1568@gmail.com>
2024-05-22 16:24:29 +0000

    later

--- into-the-lmao/src/slides.svelte
+++ into-the-lmao/src/slides.svelte
@@ -11,14 +11,20 @@
 			<section data-background-image={AnakBuanganUNAIR} data-background-repeat="repeat" />
 			<section data-background-image="https://mir-s3-cdn-cf.behance.net/project_modules/hd/d683fb40496909.5782192e0abaf.gif" data-background-repeat="repeat" />
 			<section data-background-image="https://assets.materialup.com/uploads/6102cce0-dc3c-42a3-ba0e-84d25f8a7cd3/preview.gif" data-background-repeat="repeat">
-				<p style="font-size: 3rem; text-transform:none; color:black">E-Commerce</p>
+				<p style="font-size: 3rem; text-transform:none; color:black">
+					E-Commerce
+				</p>
 			</section>
 			<section data-background-image="https://cdn.dribbble.com/users/1144520/screenshots/2908827/google-question.gif" data-background-repeat="repeat" />
 			<section data-background-image="https://cdn.dribbble.com/users/73551/screenshots/3465079/ecommerce-gif.gif" data-background-repeat="repeat">
-				<p style="font-size: 3rem; text-transform:none; color:black">Suatu kegiatan membeli ataupun menjual barang & jasa melalui internet</p>
+				<p style="font-size: 3rem; text-transform:none; color:black">
+					Suatu kegiatan membeli ataupun menjual barang & jasa melalui internet
+				</p>
 			</section>
 			<section data-background-image="https://i.pinimg.com/originals/99/b1/df/99b1dffd522e339cbf9ee35cb40ff36c.gif" data-background-repeat="repeat">
-				<p style="font-size: 3rem; text-transform:none; color:black">Contoh:</p>
+				<p style="font-size: 3rem; text-transform:none; color:black">
+					Contoh:
+				</p>
 			</section>
 		  </section>
 		  	<!--perkenalan berhenti disini-->
@@ -33,8 +39,28 @@
 			<!--mulai pembahasan Shopify-->
 		  <section>
 			<section data-background-image={AnakBuanganUNAIR} data-background-repeat="repeat" />
-			<section data-background-iframe="https://www.shopify.com/id"></section>
+			<section data-background-image="https://64.media.tumblr.com/9021802703768c4094f753b6166058d9/ba90aa8ed3e1e9de-c7/s1280x1920/7e6e36cd356a06ad192f88e81f130224ce812529.gifv" data-background-repeat="repeat">
+				<h1 style="font-size: 3rem; text-transform:none; color:black">
+					Shopify adalah platform e-commerce komprehensif yang memungkinkan individu dan bisnis untuk membuat, menyesuaikan, dan mengelola toko online mereka sendiri
+				</h1>
+			</section>
+			<!--reminder untuk menitikberatkan ke tahun sebelum 2006-->
+			<!--for goodness gracious, internet belum semodern sekarang-->
+			<section data-background-image="https://i.pinimg.com/originals/3d/17/cb/3d17cb7391b0d9cb7d669c20c23a4230.gif" data-background-repeat="repeat" />
+			<section data-background-image="https://i.pinimg.com/736x/16/62/b7/1662b745af92a46313b8314f4813b282.jpg" />
+			<section data-background-image="https://i.insider.com/513cbd916bb3f76c45000019?width=1024&format=jpeg" />
+			<!--reminder untuk menitikberatkan ke tahun sebelum 2006-->
+			<!--for goodness gracious, internet belum semodern sekarang-->
 		  </section>
+
+		  <section>
+			<section data-background-image="https://i.gifer.com/7JM3.gif" class="bg-contain">
+				<p style="3rem; text-transform:none; color:black">
+					Deutsche Qualität
+				</p>
+			</section>
+		  </section>
+		  <!--berlanjut-->
 		</div>
 	</div>
 </Presentation>
