af6dfebddb208d38e9c7a28c937964105952e2ee
the nothing <mydeadass1568@gmail.com>
2024-05-22 13:43:44 +0000

    berlanjut

---
+++ into-the-lmao/src/slides.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Presentation } from '@components';
+	import AnakBuanganUNAIR from '../src/public/iya.gif'
+</script>
+
+<Presentation>
+	<div class="reveal">
+		<div class="slides">
+			<!--perkenalan start disini-->
+		  <section>
+			<section data-background-image={AnakBuanganUNAIR} data-background-repeat="repeat" />
+			<section data-background-image="https://mir-s3-cdn-cf.behance.net/project_modules/hd/d683fb40496909.5782192e0abaf.gif" data-background-repeat="repeat" />
+			<section data-background-image="https://assets.materialup.com/uploads/6102cce0-dc3c-42a3-ba0e-84d25f8a7cd3/preview.gif" data-background-repeat="repeat">
+				<p style="font-size: 3rem; text-transform:none; color:black">E-Commerce</p>
+			</section>
+			<section data-background-image="https://cdn.dribbble.com/users/1144520/screenshots/2908827/google-question.gif" data-background-repeat="repeat" />
+			<section data-background-image="https://cdn.dribbble.com/users/73551/screenshots/3465079/ecommerce-gif.gif" data-background-repeat="repeat">
+				<p style="font-size: 3rem; text-transform:none; color:black">Suatu kegiatan membeli ataupun menjual barang & jasa melalui internet</p>
+			</section>
+			<section data-background-image="https://i.pinimg.com/originals/99/b1/df/99b1dffd522e339cbf9ee35cb40ff36c.gif" data-background-repeat="repeat">
+				<p style="font-size: 3rem; text-transform:none; color:black">Contoh:</p>
+			</section>
+		  </section>
+		  	<!--perkenalan berhenti disini-->
+
+		  	<!--contoh-contoh start disini-->
+		  <section>
+			<section data-background-image="https://blog.sociallyin.com/hs-fs/hubfs/GIF%20of%20the%20online%20platform%20TikTok%20Shop.gif?width=1889&height=852&name=GIF%20of%20the%20online%20platform%20TikTok%20Shop.gif" data-background-repeat="repeat" />
+			<section data-background-image="https://c.tenor.com/REmnLW7fuIYAAAAC/tenor.gif" data-background-repeat="repeat" />
+		  </section>
+		  	<!--contoh-contoh stop disini-->
+
+			<!--mulai pembahasan Shopify-->
+		  <section>
+			<section data-background-image={AnakBuanganUNAIR} data-background-repeat="repeat" />
+			<section data-background-iframe="https://www.shopify.com/id"></section>
+		  </section>
+		</div>
+	</div>
+</Presentation>
